19b26a1626376698073bcb7f8ff1eda2b25d7c9d
Kei-gits <kioo-17@hotmail.com>
2024-07-23 23:03:30 +0200

    Tutorial angular.dev

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,6 @@
 import {Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
-import {DetailsComponent} from "./details/details/details.component";
+import {DetailsComponent} from "./details/details.component";
 
 export const routeConfig: Routes = [
     {
